551995831d9122b9c67cc678b9b01822ccf9c70e
Junior Hirwa <iamhirwejr@gmail.com>
2025-05-08 13:52:07 +0200

    refactor: show proper imports

--- flutter_locale/lib/main.dart
+++ flutter_locale/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_locale/core/di/locator.dart';
 import 'package:flutter_locale/pages/home_page.dart';
-import 'package:flutter_localizations/flutter_localizations.dart' show GlobalCupertinoLocalizations, GlobalMaterialLocalizations, GlobalWidgetsLocalizations;
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'cubits/localization_cubit.dart';
 
